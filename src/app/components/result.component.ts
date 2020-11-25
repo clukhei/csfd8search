ef875a0dfba2fee6f730007fc72e96c702ecc0e8
Lu Khei <kheiboardwarrior@gmail.com>
2020-11-25 23:01:19 +0800

    fetch api, css display of mangas

--- src/app/components/result.component.ts
+++ src/app/components/result.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {HttpClient, HttpClientModule} from '@angular/common/http'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
@@ -8,16 +9,28 @@ import {HttpClient, HttpClientModule} from '@angular/common/http'
 })
 export class ResultComponent implements OnInit {
 
-  constructor(private router: Router, private activatedRoute:ActivatedRoute, private http: HttpClient) { }
-  searchedGenre: String =''
-  searchedQ: String =''
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+  searchedGenre: String = ''
+  searchedQ: String = ''
+  jikanResponse: Object = {}
+  results: any[] = []
+  noResults: String
+
   ngOnInit(): void {
- 
+
     this.searchedGenre = this.activatedRoute.snapshot.params.genre
     this.searchedQ = this.activatedRoute.snapshot.params.q
-    this.http.get('')
+    this.jikanResponse = this.http.get(`https://api.jikan.moe/v3/search/${this.searchedGenre}?q=${this.searchedQ}`)
+      .toPromise()
+      .then(res => {
+        this.results = res.results
+      })
+      .catch(e => console.log(e))
+
   }
-  backToForm(){
+  backToForm() {
     this.router.navigate(['/searchform'])
   }
+
+
 }
